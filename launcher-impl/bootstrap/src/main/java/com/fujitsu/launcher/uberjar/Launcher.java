0d046daefca8cb2d480850b24fbe0c425bf2872d
Takahiro Nagao <nagao.takahiro@jp.fujitsu.com>
2019-10-23 21:33:46 +0900

    Bump glassfish to 5.1

---
+++ launcher-impl/bootstrap/src/main/java/com/fujitsu/launcher/uberjar/Launcher.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright (c) 2017-2019 Fujitsu Limited and/or its affiliates. All rights
+ * reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package com.fujitsu.launcher.uberjar;
+
+import com.fujitsu.launcher.DeployProperties;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.glassfish.embeddable.GlassFish;
+import org.glassfish.embeddable.GlassFishProperties;
+import org.glassfish.embeddable.GlassFishRuntime;
+
+/**
+ *
+ * @author Tsuyoshi Yoshitomi
+ */
+public class Launcher {
+
+    public static void main(String[] args) {
+        new Launcher().start();
+    }
+
+    private void start() {
+        try (
+                InputStream gpis = this.getClass().getClassLoader().getResourceAsStream("uber-jar_glassfish.properties");
+                InputStream dpis = this.getClass().getClassLoader().getResourceAsStream("uber-jar_deploy.properties");
+                InputStream wis = this.getClass().getClassLoader().getResourceAsStream("uber-jar_application.war")) {
+
+            GlassFishProperties glassfishProperties = new GlassFishProperties();
+            glassfishProperties.getProperties().load(gpis);
+            URL url = this.getClass().getClassLoader().getResource("uber-jar_domain.xml");
+            if (url != null) {
+                glassfishProperties.setConfigFileURI(url.toURI().toString());
+            }
+
+            DeployProperties deployProperties = new DeployProperties();
+            deployProperties.load(dpis);
+
+            GlassFish glassfish = GlassFishRuntime.bootstrap().newGlassFish(glassfishProperties);
+            glassfish.start();
+            glassfish.getDeployer().deploy(wis, deployProperties.getDeployOptions());
+        } catch (Throwable th) {
+            Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "Server was stopped.", th);
+            System.exit(1);
+        }
+    }
+}

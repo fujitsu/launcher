01a67da24e71ee6bec22d4e96ea6181ec969990e
dsano82 <sano.daiki@fujitsu.com>
2021-10-22 13:54:59 +0900

    Update MP FT version to 3.0

---
+++ launcher-impl/microprofile/fault-tolerance/src/main/java/com/fujitsu/launcher/microprofile/faulttolerance/ThreadFactoryProvider.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2021 Fujitsu Limited and/or its affiliates. All rights
+ * reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0, which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package com.fujitsu.launcher.microprofile.faulttolerance;
+
+import java.util.concurrent.ThreadFactory;
+
+import javax.enterprise.concurrent.ManagedThreadFactory;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+@Singleton
+public class ThreadFactoryProvider {
+    private ManagedThreadFactory factory;
+
+    @Inject
+    public ThreadFactoryProvider(
+        @ConfigProperty(name = "com.fujitsu.launcher.microprofile.faulttolerance.managedThreadFactory", defaultValue="java:comp/DefaultManagedThreadFactory") String factoryName
+    ){
+        try{
+            InitialContext ic = new InitialContext();
+            factory = (ManagedThreadFactory) ic.lookup(factoryName);
+        } catch (NamingException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ThreadFactory get(){
+        return factory;
+    }
+}

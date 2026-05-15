56bebc8efc4cd81086a3fbed4f9308fdbc0fb723
Takahiro Nagao <nagao.takahiro@fujitsu.com>
2026-05-15 09:35:59 +0900

    Fix for ServletRequest.getReader().readLine() problem

--- launcher-impl/glassfish/src/main/java/org/apache/catalina/connector/CoyoteReader.java
+++ launcher-impl/glassfish/src/main/java/org/apache/catalina/connector/CoyoteReader.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2026 Fujitsu Limited.
  * Copyright (c) 1997-2018 Oracle and/or its affiliates. All rights reserved.
  * Copyright 2004 The Apache Software Foundation
  *
@@ -195,7 +196,7 @@ public class CoyoteReader
         StringBuilder aggregator = null;
         while (end < 0) {
             mark(MAX_LINE_LENGTH);
-            while ((pos < MAX_LINE_LENGTH) && (end < 0)) {
+            while ((pos < MAX_LINE_LENGTH - 1) && (end < 0)) {
                 int nRead = read(lineBuffer, pos, MAX_LINE_LENGTH - pos);
                 if (nRead < 0) {
                     if (pos == 0 && aggregator == null) {
@@ -230,7 +231,7 @@ public class CoyoteReader
                 if (aggregator == null) {
                     aggregator = new StringBuilder();
                 }
-                aggregator.append(lineBuffer);
+                aggregator.append(lineBuffer, 0, pos);
                 pos = 0;
             } else {
                 reset();
